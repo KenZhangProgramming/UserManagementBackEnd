411a53a4ad9f3c8dabfa53517a7e59ca4f904a6b
Yukang Ken Zhang <yukang.ken.zhang@avanade.com>
2020-04-05 18:14:39 -0400

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagementBackEnd.Models
+{
+    public class Customer
+    {   public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public Province Province { get; set; }
+        public int StateId { get; set; }
+        public int Zip { get; set; }
+        public string Gender { get; set; }
+        public int OrderCount { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
